3aee4236662da166196277df4d31b2566cdf26be
Grahit6583 <grahitgoyal4545@gmail.com>
2026-09-06 17:35:34 +0530

    Initial Spring Boot project setup

---
+++ railway-schedular/railway-schedular/src/main/java/com/trackflow/railway_schedular/RailwaySchedularApplication.java
@@ -0,0 +1,13 @@
+package com.trackflow.railway_schedular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwaySchedularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailwaySchedularApplication.class, args);
+	}
+
+}
---
+++ railway-schedular/railway-schedular/src/test/java/com/trackflow/railway_schedular/RailwaySchedularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trackflow.railway_schedular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailwaySchedularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
